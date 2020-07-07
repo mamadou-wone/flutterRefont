8460525786a5c8a579b5ed2827e0b7013976fb31
Mamadou WONE <mamadouwone12345@gmail.com>
2020-07-07 18:38:24 +0000

    Day17 #100DaysOfCode Part2 Build MagicBall

--- lib/main.dart
+++ lib/main.dart
@@ -7,66 +7,102 @@ void main() {
     MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.blue[400],
         appBar: AppBar(
-          title: Text('Dicee'),
-          backgroundColor: Colors.red,
+          title: Text(
+            'Magic Ball',
+            style: TextStyle(
+              fontSize: 25,
+            ),
+          ),
+          backgroundColor: Colors.blue[800],
           centerTitle: true,
         ),
-        body: DicePage(),
+        body: MagicBall(),
       ),
     ),
   );
 }
 
-class DicePage extends StatefulWidget {
+class MagicBall extends StatefulWidget {
   @override
-  _DicePageState createState() => _DicePageState();
+  _MagicBallState createState() => _MagicBallState();
 }
 
-class _DicePageState extends State<DicePage> {
-  List<String> DiceNumber = [
-    'dice1.png',
-    'dice2.png',
-    'dice3.png',
-    'dice4.png',
-    'dice5.png',
-    'dice6.png'
-  ];
+class _MagicBallState extends State<MagicBall> {
+  var magicBallNumber = 1;
+  void MagicBallGenerate() {
+    setState(() {
+      magicBallNumber = Random().nextInt(5) + 1;
+    });
+  }
 
-  var diceNumberGenerate = new Random();
-  var diceNumberGenerate2 = new Random();
-
-  String diceImage = 'dice1.png';
-  String diceImage2 = 'dice1.png';
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-          child: GestureDetector(
-        child: Row(
-          children: <Widget>[
-            Expanded(
-              child: Padding(
-                child: Image.asset('images/' + diceImage),
-                padding: EdgeInsets.all(15.0),
-              ),
-            ),
-            Expanded(
-              child: Padding(
-                child: Image.asset('images/' + diceImage2),
-                padding: EdgeInsets.all(15.0),
-              ),
-            ),
-          ],
+    return Center(
+      child: Expanded(
+        child: IconButton(
+          icon: Image.asset('images/ball$magicBallNumber.png'),
+          iconSize: 500,
+          onPressed: () {
+            setState(() {
+              MagicBallGenerate();
+            });
+          },
         ),
-        onTap: () {
-          setState(() {
-            diceImage = DiceNumber[diceNumberGenerate.nextInt(6)];
-            diceImage2 = DiceNumber[diceNumberGenerate2.nextInt(6)];
-          });
-        },
-      )),
+      ),
     );
   }
 }
+
+// class DicePage extends StatefulWidget {
+//   @override
+//   _DicePageState createState() => _DicePageState();
+// }
+
+// class _DicePageState extends State<DicePage> {
+//   List<String> DiceNumber = [
+//     'dice1.png',
+//     'dice2.png',
+//     'dice3.png',
+//     'dice4.png',
+//     'dice5.png',
+//     'dice6.png'
+//   ];
+
+//   var diceNumberGenerate = new Random();
+//   var diceNumberGenerate2 = new Random();
+
+//   String diceImage = 'dice1.png';
+//   String diceImage2 = 'dice1.png';
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       child: Center(
+//           child: GestureDetector(
+//         child: Row(
+//           children: <Widget>[
+//             Expanded(
+//               child: Padding(
+//                 child: Image.asset('images/' + diceImage),
+//                 padding: EdgeInsets.all(15.0),
+//               ),
+//             ),
+//             Expanded(
+//               child: Padding(
+//                 child: Image.asset('images/' + diceImage2),
+//                 padding: EdgeInsets.all(15.0),
+//               ),
+//             ),
+//           ],
+//         ),
+//         onTap: () {
+//           setState(() {
+//             diceImage = DiceNumber[diceNumberGenerate.nextInt(6)];
+//             diceImage2 = DiceNumber[diceNumberGenerate2.nextInt(6)];
+//           });
+//         },
+//       )),
+//     );
+//   }
+// }
