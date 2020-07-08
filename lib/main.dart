326008fb73d25aec0d4ea8b714fa68c24ab8422a
Mamadou WONE <mamadouwone12345@gmail.com>
2020-07-08 18:32:32 +0000

    Day18 #100DaysOfCode [18/100] XylophoneApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
-import 'dart:math';
+// import 'dart:math';
 
+import 'package:audioplayers/audio_cache.dart';
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,102 +9,54 @@ void main() {
     MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.blue[400],
         appBar: AppBar(
           title: Text(
-            'Magic Ball',
+            'Xylophone',
             style: TextStyle(
               fontSize: 25,
+              fontFamily: 'Pacifico',
             ),
           ),
-          backgroundColor: Colors.blue[800],
+          // backgroundColor: Colors.blue[800],
           centerTitle: true,
         ),
-        body: MagicBall(),
+        body: Xylophone(),
       ),
     ),
   );
 }
 
-class MagicBall extends StatefulWidget {
+class Xylophone extends StatefulWidget {
   @override
-  _MagicBallState createState() => _MagicBallState();
+  _XylophoneState createState() => _XylophoneState();
 }
 
-class _MagicBallState extends State<MagicBall> {
-  var magicBallNumber = 1;
-  void MagicBallGenerate() {
-    setState(() {
-      magicBallNumber = Random().nextInt(5) + 1;
-    });
+class _XylophoneState extends State<Xylophone> {
+  AudioCache audioCache = AudioCache();
+  AudioPlayer advancedPlayer = AudioPlayer();
+
+  Player(Color color, String note) {
+    return Expanded(
+      child: FlatButton(
+        color: color,
+        onPressed: () => audioCache.play(note),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Expanded(
-        child: IconButton(
-          icon: Image.asset('images/ball$magicBallNumber.png'),
-          iconSize: 500,
-          onPressed: () {
-            setState(() {
-              MagicBallGenerate();
-            });
-          },
-        ),
-      ),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Player(Colors.red, 'note1.wav'),
+        Player(Colors.yellow, 'note2.wav'),
+        Player(Colors.green, 'note3.wav'),
+        Player(Colors.teal[900], 'note4.wav'),
+        Player(Colors.blue[900], 'note5.wav'),
+        Player(Colors.purple, 'note6.wav'),
+        Player(Colors.white, 'note7.wav'),
+      ],
     );
   }
 }
-
-// class DicePage extends StatefulWidget {
-//   @override
-//   _DicePageState createState() => _DicePageState();
-// }
-
-// class _DicePageState extends State<DicePage> {
-//   List<String> DiceNumber = [
-//     'dice1.png',
-//     'dice2.png',
-//     'dice3.png',
-//     'dice4.png',
-//     'dice5.png',
-//     'dice6.png'
-//   ];
-
-//   var diceNumberGenerate = new Random();
-//   var diceNumberGenerate2 = new Random();
-
-//   String diceImage = 'dice1.png';
-//   String diceImage2 = 'dice1.png';
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//       child: Center(
-//           child: GestureDetector(
-//         child: Row(
-//           children: <Widget>[
-//             Expanded(
-//               child: Padding(
-//                 child: Image.asset('images/' + diceImage),
-//                 padding: EdgeInsets.all(15.0),
-//               ),
-//             ),
-//             Expanded(
-//               child: Padding(
-//                 child: Image.asset('images/' + diceImage2),
-//                 padding: EdgeInsets.all(15.0),
-//               ),
-//             ),
-//           ],
-//         ),
-//         onTap: () {
-//           setState(() {
-//             diceImage = DiceNumber[diceNumberGenerate.nextInt(6)];
-//             diceImage2 = DiceNumber[diceNumberGenerate2.nextInt(6)];
-//           });
-//         },
-//       )),
-//     );
-//   }
-// }
