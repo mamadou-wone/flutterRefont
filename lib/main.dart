270e05c086d2cb85c321cd906927e187a36126de
Mamadou WONE <mamadouwone12345@gmail.com>
2020-07-09 19:23:48 +0000

    Day19 #100DaysOfCode [19/100]

--- lib/main.dart
+++ lib/main.dart
@@ -1,62 +1,129 @@
-// import 'dart:math';
-
-import 'package:audioplayers/audio_cache.dart';
-import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  return runApp(
-    MaterialApp(
+void main() => runApp(Quizzler());
+
+class Quizzler extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'Xylophone',
-            style: TextStyle(
-              fontSize: 25,
-              fontFamily: 'Pacifico',
-            ),
+        backgroundColor: Colors.grey.shade900,
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
           ),
-          // backgroundColor: Colors.blue[800],
-          centerTitle: true,
         ),
-        body: Xylophone(),
-      ),
-    ),
-  );
-}
-
-class Xylophone extends StatefulWidget {
-  @override
-  _XylophoneState createState() => _XylophoneState();
-}
-
-class _XylophoneState extends State<Xylophone> {
-  AudioCache audioCache = AudioCache();
-  AudioPlayer advancedPlayer = AudioPlayer();
-
-  Player(Color color, String note) {
-    return Expanded(
-      child: FlatButton(
-        color: color,
-        onPressed: () => audioCache.play(note),
       ),
     );
   }
+}
 
+class QuizPage extends StatefulWidget {
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = [];
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.'
+  ];
+  int questionIndex = 0;
+  // String question =  questions[questionIndex];
   @override
   Widget build(BuildContext context) {
     return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: <Widget>[
-        Player(Colors.red, 'note1.wav'),
-        Player(Colors.yellow, 'note2.wav'),
-        Player(Colors.green, 'note3.wav'),
-        Player(Colors.teal[900], 'note4.wav'),
-        Player(Colors.blue[900], 'note5.wav'),
-        Player(Colors.purple, 'note6.wav'),
-        Player(Colors.white, 'note7.wav'),
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                questions[questionIndex],
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              textColor: Colors.white,
+              color: Colors.green,
+              child: Text(
+                'True',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+              onPressed: () {
+                //The user picked true.
+                setState(() {
+                  questionIndex++;
+                  questions[questionIndex];
+                  scoreKeeper.add(
+                    Icon(
+                      Icons.check,
+                      color: Colors.green,
+                    ),
+                  );
+                });
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              color: Colors.red,
+              child: Text(
+                'False',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: () {
+                //The user picked false.
+                setState(() {
+                  questionIndex++;
+                  questions[questionIndex];
+                  scoreKeeper.add(
+                    Icon(
+                      Icons.close,
+                      color: Colors.red,
+                    ),
+                  );
+                });
+              },
+            ),
+          ),
+        ),
+        //TODO: Add a Row here as your score keeper
+        Row(
+          children: scoreKeeper,
+        ),
       ],
     );
   }
 }
+
+/*
+question1: 'You can lead a cow down stairs but not up stairs.', false,
+question2: 'Approximately one quarter of human bones are in the feet.', true,
+question3: 'A slug\'s blood is green.', true,
+*/
