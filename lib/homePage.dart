4261057c899bd4427ed02b8d9a3b1635d466f1e2
Mamadou WONE <mamadouwone12345@gmail.com>
2020-07-15 18:12:13 +0000

    Day24 #100DaysOfCode [24/100]

---
+++ lib/homePage.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Stack(
+        children: <Widget>[
+          Container(
+            width: 250.0,
+            height: 250.0,
+            color: Colors.white,
+          ),
+          Container(
+            padding: EdgeInsets.all(5.0),
+            alignment: Alignment.bottomCenter,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: <Color>[
+                  Colors.black.withAlpha(0),
+                  Colors.black12,
+                  Colors.black45
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:TrainingApp/homePage.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  final _textController = TextEditingController();
-
-  void _handleSubmitted(String text) {
-    _textController.clear();
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'ChattApp',
       home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.red[400],
-          title: Text('ChatApp'),
-          centerTitle: true,
-        ),
-        body: Container(
-          margin: EdgeInsets.symmetric(horizontal: 8.0),
-          child: Row(
-            children: [
-              Flexible(
-                child: TextField(
-                  controller: _textController,
-                  onSubmitted: _handleSubmitted,
-                  decoration:
-                      InputDecoration.collapsed(hintText: 'Send a message'),
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.symmetric(horizontal: 4.0),
-                child: IconButton(
-                    icon: const Icon(
-                      Icons.send,
-                      color: Colors.teal,
-                    ),
-                    onPressed: () => _handleSubmitted(_textController.text)),
-              ),
-            ],
-          ),
-        ),
+        body: HomePage(),
       ),
     );
   }
