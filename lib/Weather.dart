9e127002f22f17311a94f9db94fe4300c15d39a7
Mamadou WONE <mamadouwone12345@gmail.com>
2020-07-19 14:13:36 +0000

    Day27 #100DaysOfCode [27/100]

--- lib/Weather.dart
+++ lib/Weather.dart
@@ -1,83 +1,83 @@
-import 'dart:async';
-import 'dart:convert';
+// import 'dart:async';
+// import 'dart:convert';
 
-import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+// import 'package:flutter/foundation.dart';
+// import 'package:flutter/material.dart';
+// import 'package:http/http.dart' as http;
 
-Future<Weather> fetchWeather() async {
-  final response = await http.get(
-      'http://api.openweathermap.org/data/2.5/weather?q=Dakar&appid=d25716d961a86dd8ad7a074fd248ff59&units=metric');
+// Future<Weather> fetchWeather() async {
+//   final response = await http.get(
+//       'http://api.openweathermap.org/data/2.5/weather?q=Dakar&appid=d25716d961a86dd8ad7a074fd248ff59&units=metric');
 
-  if (response.statusCode == 200) {
-    // If the server did return a 200 OK response,
-    // then parse the JSON.
-    return Weather.fromJson(json.decode(response.body));
-  } else {
-    // If the server did not return a 200 OK response,
-    // then throw an exception.
-    throw Exception('Failed to load album');
-  }
-}
+//   if (response.statusCode == 200) {
+//     // If the server did return a 200 OK response,
+//     // then parse the JSON.
+//     return Weather.fromJson(json.decode(response.body));
+//   } else {
+//     // If the server did not return a 200 OK response,
+//     // then throw an exception.
+//     throw Exception('Failed to load album');
+//   }
+// }
 
-class Weather {
-  final int maxTemp;
+// class Weather {
+//   final int maxTemp;
 
-  Weather({this.maxTemp});
-  factory Weather.fromJson(Map<String, dynamic> json) {
-    return Weather(maxTemp: json['maxTemp']);
-  }
+//   Weather({this.maxTemp});
+//   factory Weather.fromJson(Map<String, dynamic> json) {
+//     return Weather(maxTemp: json['maxTemp']);
+//   }
 
-  // get main => null;
-}
+//   // get main => null;
+// }
 
-class Meteo extends StatefulWidget {
-  Meteo({Key key}) : super(key: key);
+// class Meteo extends StatefulWidget {
+//   Meteo({Key key}) : super(key: key);
 
-  @override
-  _MeteoState createState() => _MeteoState();
-}
+//   @override
+//   _MeteoState createState() => _MeteoState();
+// }
 
-class _MeteoState extends State<Meteo> {
-  Future<Weather> futureWeather;
+// class _MeteoState extends State<Meteo> {
+//   Future<Weather> futureWeather;
 
-  @override
-  void initState() {
-    super.initState();
-    futureWeather = fetchWeather();
-  }
+//   @override
+//   void initState() {
+//     super.initState();
+//     futureWeather = fetchWeather();
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Fetch Data Example',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Fetch Data Example'),
-        ),
-        body: Center(
-          child: FutureBuilder<Weather>(
-            future: futureWeather,
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return Text(
-                  // var temp = data['main']['temp_max'];
-                  snapshot.data.,
-                  style: TextStyle(fontSize: 30.0),
-                );
-              } else if (snapshot.hasError) {
-                return Text("${snapshot.error}");
-              }
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Fetch Data Example',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Text('Fetch Data Example'),
+//         ),
+//         body: Center(
+//           child: FutureBuilder<Weather>(
+//             future: futureWeather,
+//             builder: (context, snapshot) {
+//               if (snapshot.hasData) {
+//                 return Text(
+//                   // var temp = data['main']['temp_max'];
+//                   snapshot.data.,
+//                   style: TextStyle(fontSize: 30.0),
+//                 );
+//               } else if (snapshot.hasError) {
+//                 return Text("${snapshot.error}");
+//               }
 
-              // By default, show a loading spinner.
-              return CircularProgressIndicator();
-            },
-          ),
-        ),
-      ),
-    );
-  }
-}
+//               // By default, show a loading spinner.
+//               return CircularProgressIndicator();
+//             },
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,43 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Meteo(),
+      home: Thioro(),
+    );
+  }
+}
+
+class Thioro extends StatefulWidget {
+  @override
+  _ThioroState createState() => _ThioroState();
+}
+
+class _ThioroState extends State<Thioro> {
+  void thioro() {
+    setState(() {
+      scaffoldColor = Colors.red;
+      text = 'Thioro tu doit chercher une puce 4G';
+    });
+  }
+
+  Color color = Colors.red;
+  String text = 'Bonjour je fais du Flutter';
+  Color scaffoldColor = Colors.teal;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: scaffoldColor,
+      body: Center(
+        child: Text(
+          text,
+          style: TextStyle(fontSize: 20.0),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.flash_on),
+        onPressed: () {
+          thioro();
+        },
+      ),
     );
   }
 }
