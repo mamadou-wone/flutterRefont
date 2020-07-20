5a11e030d1de224db5cd5b8cc4843f580c70bb2d
Mamadou WONE <mamadouwone12345@gmail.com>
2020-07-20 13:27:26 +0000

    Day27 part1

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
-import 'package:TrainingApp/Picture.dart';
-import 'package:TrainingApp/Weather.dart';
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
-import 'package:TrainingApp/homePage.dart';
 
 void main() => runApp(MyApp());
 
@@ -12,43 +10,101 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Thioro(),
+      theme: ThemeData(
+        primaryColor: Colors.red,
+      ),
+      home: Training(),
     );
   }
 }
 
-class Thioro extends StatefulWidget {
+class Training extends StatefulWidget {
   @override
-  _ThioroState createState() => _ThioroState();
+  _TrainingState createState() => _TrainingState();
 }
 
-class _ThioroState extends State<Thioro> {
-  void thioro() {
-    setState(() {
-      scaffoldColor = Colors.red;
-      text = 'Thioro tu doit chercher une puce 4G';
-    });
+class _TrainingState extends State<Training> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = TextStyle(fontSize: 18.0);
+  final _saved = Set<WordPair>();
+
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute(builder: (BuildContext context) {
+        final tiles = _saved.map((WordPair pair) {
+          return ListTile(
+            title: Text(
+              pair.asPascalCase,
+              style: _biggerFont,
+            ),
+          );
+        });
+        final divided = ListTile.divideTiles(
+          context: context,
+          tiles: tiles,
+        ).toList();
+        return Scaffold(
+          appBar: AppBar(
+            title: Text('Saved Suggestions'),
+          ),
+          body: ListView(children: divided),
+        );
+      }),
+    );
   }
 
-  Color color = Colors.red;
-  String text = 'Bonjour je fais du Flutter';
-  Color scaffoldColor = Colors.teal;
+  Widget _buildSeggestion() {
+    return ListView.builder(
+        padding: EdgeInsets.all(16.0),
+        itemBuilder: /*1*/ (context, i) {
+          if (i.isOdd) return Divider(); /*2*/
+
+          final index = i ~/ 2; /*3*/
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+          }
+          return GestureDetector(
+            child: _buildRow(_suggestions[index]),
+            onTap: () {
+              print(_suggestions[index]);
+            },
+          );
+        });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: scaffoldColor,
-      body: Center(
-        child: Text(
-          text,
-          style: TextStyle(fontSize: 20.0),
-        ),
+      appBar: AppBar(
+        title: Text('EnglishWORD'),
+        actions: [
+          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.flash_on),
-        onPressed: () {
-          thioro();
-        },
-      ),
+      body: _buildSeggestion(),
     );
   }
 }
