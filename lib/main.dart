c29963e1c73b277cccc8f9642b65384c1c2250b6
Mamadou WONE <mamadouwone12345@gmail.com>
2020-08-01 17:39:30 +0000

    Day38 #100DaysOfCode [38/100]

--- lib/main.dart
+++ lib/main.dart
@@ -3,117 +3,127 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-final dummySnapshot = [
-  {"name": "Mégatron", "votes": 15},
-  {"name": "Baba", "votes": 14},
-  {"name": "Badou", "votes": 11},
-  {"name": "Laye", "votes": 10},
-  {"name": "Elzo", "votes": 1},
-];
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Baby Names',
-      home: MyHomePage(),
+      home: Scaffold(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() {
-    return _MyHomePageState();
-  }
-}
+// final dummySnapshot = [
+//   {"name": "Mégatron", "votes": 15},
+//   {"name": "Baba", "votes": 14},
+//   {"name": "Badou", "votes": 11},
+//   {"name": "Laye", "votes": 10},
+//   {"name": "Elzo", "votes": 1},
+// ];
 
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blueGrey[700],
-        centerTitle: true,
-        title: Text(
-          'BabyBoom',
-          style: TextStyle(
-              fontFamily: 'Roboto',
-              fontSize: 25.0,
-              fontWeight: FontWeight.bold,
-              color: Colors.white),
-        ),
-        actions: <Widget>[
-          Icon(
-            Icons.settings,
-            color: Colors.white,
-            size: 25.0,
-          )
-        ],
-      ),
-      body: _buildBody(context),
-    );
-  }
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       title: 'Baby Names',
+//       home: MyHomePage(),
+//     );
+//   }
+// }
 
-  Widget _buildBody(BuildContext context) {
-    return StreamBuilder<QuerySnapshot>(
-      stream: Firestore.instance.collection('baby').snapshots(),
-      builder: (context, snapshot) {
-        if (!snapshot.hasData) return LinearProgressIndicator();
+// class MyHomePage extends StatefulWidget {
+//   @override
+//   _MyHomePageState createState() {
+//     return _MyHomePageState();
+//   }
+// }
 
-        return _buildList(context, snapshot.data.documents);
-      },
-    );
-  }
+// class _MyHomePageState extends State<MyHomePage> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         backgroundColor: Colors.blueGrey[700],
+//         centerTitle: true,
+//         title: Text(
+//           'BabyBoom',
+//           style: TextStyle(
+//               fontFamily: 'Roboto',
+//               fontSize: 25.0,
+//               fontWeight: FontWeight.bold,
+//               color: Colors.white),
+//         ),
+//         actions: <Widget>[
+//           Icon(
+//             Icons.settings,
+//             color: Colors.white,
+//             size: 25.0,
+//           )
+//         ],
+//       ),
+//       body: _buildBody(context),
+//     );
+//   }
 
-  Widget _buildList(BuildContext context, List<DocumentSnapshot> snapshot) {
-    return ListView(
-      padding: const EdgeInsets.only(top: 20.0),
-      children: snapshot.map((data) => _buildListItem(context, data)).toList(),
-    );
-  }
+//   Widget _buildBody(BuildContext context) {
+//     return StreamBuilder<QuerySnapshot>(
+//       stream: Firestore.instance.collection('baby').snapshots(),
+//       builder: (context, snapshot) {
+//         if (!snapshot.hasData) return LinearProgressIndicator();
 
-  Widget _buildListItem(BuildContext context, DocumentSnapshot data) {
-    final record = Record.fromSnapshot(data);
-    return Padding(
-      key: ValueKey(record.name),
-      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-      child: Container(
-        decoration: BoxDecoration(
-          border: Border.all(color: Colors.grey),
-          borderRadius: BorderRadius.circular(5.0),
-        ),
-        child: ListTile(
-          title: Text(record.name),
-          trailing: Text(record.votes.toString()),
-          onTap: () =>
-              record.reference.updateData({'votes': FieldValue.increment(1)}),
-        ),
-      ),
-    );
-  }
-}
+//         return _buildList(context, snapshot.data.documents);
+//       },
+//     );
+//   }
 
-// NULL null
-class Record {
-  // final int test;
-  final String name;
-  final int votes;
-  final DocumentReference reference;
+//   Widget _buildList(BuildContext context, List<DocumentSnapshot> snapshot) {
+//     return ListView(
+//       padding: const EdgeInsets.only(top: 20.0),
+//       children: snapshot.map((data) => _buildListItem(context, data)).toList(),
+//     );
+//   }
 
-  Record.fromMap(Map<String, dynamic> map, {this.reference})
-      : assert(map['name'] != null),
-        assert(map['votes'] != null),
-        name = map['name'],
-        votes = map['votes'];
+//   Widget _buildListItem(BuildContext context, DocumentSnapshot data) {
+//     final record = Record.fromSnapshot(data);
+//     return Padding(
+//       key: ValueKey(record.name),
+//       padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+//       child: Container(
+//         decoration: BoxDecoration(
+//           border: Border.all(color: Colors.grey),
+//           borderRadius: BorderRadius.circular(5.0),
+//         ),
+//         child: ListTile(
+//           title: Text(record.name),
+//           trailing: Text(record.votes.toString()),
+//           onTap: () =>
+//               record.reference.updateData({'votes': FieldValue.increment(1)}),
+//         ),
+//       ),
+//     );
+//   }
+// }
 
-  Record.fromSnapshot(DocumentSnapshot snapshot)
-      : this.fromMap(snapshot.data, reference: snapshot.reference);
+// // NULL null
+// class Record {
+//   // final int test;
+//   final String name;
+//   final int votes;
+//   final DocumentReference reference;
 
-  @override
-  String toString() => "Record<$name:$votes>";
-}
+//   Record.fromMap(Map<String, dynamic> map, {this.reference})
+//       : assert(map['name'] != null),
+//         assert(map['votes'] != null),
+//         name = map['name'],
+//         votes = map['votes'];
+
+//   Record.fromSnapshot(DocumentSnapshot snapshot)
+//       : this.fromMap(snapshot.data, reference: snapshot.reference);
+
+//   @override
+//   String toString() => "Record<$name:$votes>";
+// }
 
 // class MyApp extends StatefulWidget {
 //   // GOOD
